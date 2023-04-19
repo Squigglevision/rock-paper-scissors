f2809806fb9fad39f232a9730e9e4b4cb1b7e903
Squigglevision <127502256+Squigglevision@users.noreply.github.com>
2023-04-19 23:26:48 +1000

    Remove unnecessary test code from comments
    Cleaned up the code by removing my test code from the comments.

--- javascript.js
+++ javascript.js
@@ -1,129 +1,8 @@
 
-
-/*if (computerChoice === 0) {
-    console.log("Rock");
-}
-    else if (computerChoice === 1){
-        console.log("Paper");
-    }
-
-    else if (computerChoice === 2){
-        console.log("Scissors");
-    }
-    else {
-        console.log("Something went wrong...");
-    }
-*/
-
-
-
-/*
-if (playerChoice == "rock") {
-
-        console.log("Rock");
-
-    }
-
-    else if (playerChoice == "paper") {
-
-        console.log("Paper");
-
-    }
-   
-    else if (playerChoice == "scissors") {
-
-        console.log("Scissors");
-
-    }
-    
-    else
-        
-    {
-    console.log("That's not an option!");
-    }
-
-
-    */
-
-
-
-
-
-/* function playOneRound(playerChoice, computerChoice) {
-
-    if (playerChoice == "rock" && computerChoice === 0) {
-
-        console.log "You both picked Rock - it's a draw!"; 
-
-    }
-
-    else if (playerChoice == "rock" && computerChoice === 1) {
-        computerScore = computerScore + 1;
-        console.log "You lose! Paper beats Rock";
-
-
-    }
-
-    else if (playerChoice == "rock" && computerChoice === 2) {
-        playerScore = playerScore + 1;
-        console.log "You win! Rock beats scissors";
-
-
-    }
-
-
-    else if (playerChoice == "paper" && computerChoice === 0) {
-        playerScore = playerScore + 1; 
-        console.log "You win! Paper beats rock";
-
-    }
-
-    else if (playerChoice == "paper" && computerChoice === 1) {
-
-        console.log "You both picked paper - it's a draw!";
-
-    }
-
-    else if (playerChoice == "paper" && computerChoice === 2) {
-        computerScore = computerScore + 1;
-        console.log "You lose! scissors beats paper";
-
-
-    }
-
-    else if (playerChoice == "scissors" && computerChoice === 0) {
-        computerScore = computerScore + 1; 
-        console.log "You lose! Rock beats scissors";
-
-
-    }
-
-    else if (playerChoice == "scissors" && computerChoice === 1) {
-        playerScore = playerScore + 1; 
-        console.log "You win! Scissors beats paper";
-
-
-    }
-
-    else if (playerChoice == "scissors" && computerChoice === 2) {
-
-        console.log "You both picked scissors - it's a draw!";
-
-    }
-
-    else {
-        console.log "Something strange happened...";
-
-    }
-
-} */
-
 function getComputerChoice(max) {
     return Math.floor(Math.random() * max);
     }
     
-
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -248,7 +127,5 @@ function game() {
     }
 
     
-
-
 console.log(game());
 
