627be0c82d05a0b6d6189cbd98736daa86d3aace
Squigglevision <127502256+Squigglevision@users.noreply.github.com>
2023-05-04 12:46:39 +1000

    Add graphics and option to play again
    Added images and CSS stylings to the buttons and page. Created 'play
    again' button that will reset the scores when clicked.
    
    The button will keep adding itself, so the next step will be to
    remove event listeners when the button appears and to add them again
    when button is clicked. This should make sure the button only appears
    once.

--- javascript.js
+++ javascript.js
@@ -2,143 +2,150 @@
 function getComputerChoice(max) {
     return Math.floor(Math.random() * max);
 }
-    
-let playerScore = 0;
-let computerScore = 0;
-
-
 
 function playMatch(matchChoice, computerChoice) {
 
     if (matchChoice == "rock" && computerChoice === 0) {
 
         console.log("You both picked Rock - it's a draw!"); 
-        results.textContent = 'Result: You both picked Rock - it\'s a draw!';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
+        results.textContent = 'You both picked Rock - it\'s a draw!';
+        showScore();
 
     }
 
     else if (matchChoice == "rock" && computerChoice === 1) {
         computerScore = computerScore + 1;
-        console.log("You lose this round! Paper beats Rock");
-        results.textContent = 'Result: You lose this round! Paper beats Rock';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;win
         declareWinner();
+        console.log("You lose this round! Paper beats Rock");
+        results.textContent = 'You lose this round! Paper beats Rock';
+        showScore();
+
 
 
     }
 
     else if (matchChoice == "rock" && computerChoice === 2) {
         playerScore = playerScore + 1;
-        console.log("You min this round! Rock beats scissors");
-        results.textContent = 'Result: You win this round! Rock beats scissors';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
         declareWinner();
+        console.log("You min this round! Rock beats scissors");
+        results.textContent = 'You win this round! Rock beats scissors';
+        showScore();
+
 
     }
 
 
     else if (matchChoice == "paper" && computerChoice === 0) {
         playerScore = playerScore + 1; 
-        console.log("You min this round! Paper beats rock");
-        results.textContent = 'Result: You min this round! Paper beats rock';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
         declareWinner();
+        console.log("You min this round! Paper beats rock");
+        results.textContent = 'You min this round! Paper beats rock';
+        showScore();
+ 
 
     }
 
     else if (matchChoice == "paper" && computerChoice === 1) {
 
         console.log("You both picked paper - it's a draw!");
-        results.textContent = 'Result: You both picked paper - it\'s a draw!';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
+        results.textContent = 'You both picked paper - it\'s a draw!';
+        showScore();
 
     }
 
     else if (matchChoice == "paper" && computerChoice === 2) {
         computerScore = computerScore + 1;
-        console.log("You lose this round! scissors beats paper");
-        results.textContent = 'Result: You lose this round! scissors beats paper';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
         declareWinner();
+        console.log("You lose this round! scissors beats paper");
+        results.textContent = 'You lose this round! scissors beats paper';
+        showScore();
+
 
     }
 
     else if (matchChoice == "scissors" && computerChoice === 0) {
         computerScore = computerScore + 1; 
-        console.log("You lose this round! Rock beats scissors");
-        results.textContent = 'Result: You lose this round! Rock beats scissors';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
         declareWinner();
+        console.log("You lose this round! Rock beats scissors");
+        results.textContent = 'You lose this round! Rock beats scissors';
+        showScore();
+
 
     }
 
     else if (matchChoice == "scissors" && computerChoice === 1) {
         playerScore = playerScore + 1; 
-        console.log("You min this round! Scissors beats paper");
-        results.textContent = 'Result: You min this round! Scissors beats paper';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
         declareWinner();
+        console.log("You min this round! Scissors beats paper");
+        results.textContent = 'You min this round! Scissors beats paper';
+        showScore();
+
 
     }
 
     else if (matchChoice == "scissors" && computerChoice === 2) {
 
         console.log("You both picked scissors - it's a draw!");
-        results.textContent = 'Result: You both picked scissors - it\'s a draw!';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
+        results.textContent = 'You both picked scissors - it\'s a draw!';
+        showScore();
 
     }
 
     else {
         console.log("Something strange happened...");
-        results.textContent = 'Result: Hm... Something went wrong.';
-        score.textContent = 
-            `Score | Player: ${playerScore} 
-            Computer: ${computerScore}`;
-
+        results.textContent = 'Hm... Something went wrong.';
+        showScore();
     }
-
 }
 
 function declareWinner() {
     if (playerScore === 5 && computerScore < 5){
-
         console.log("Congratulations! You beat the computer!");
-        winner.textContent = `Congratulations! You beat the computer!`;
+        winnerText.textContent = `Congratulations! You beat the computer! \r\n`;
+        const playAgainButton = document.createElement('button');
+        winner.appendChild(playAgainButton); 
+        playAgainButton.innerText = `Play again?`;
+        playAgainButton.addEventListener('click', function(e) {
+        playAgain();
+        showScore();
+        winner.removeChild(playAgainButton);
+
+
+        });
 
     }
 
-    else if (computerScore === 5 && playerScore <5) {
+    else if (computerScore === 5 && playerScore < 5) {
         console.log("You've been bested by the computer!" 
                 + " Better luck next time...")
+        winnerText.textContent = `You've been bested by the computer!` 
+        + ` Better luck next time... \r\n`;
+        const playAgainButton = document.createElement('button');
+        winner.appendChild(playAgainButton); 
+        playAgainButton.innerText = `Play again?`;
+        playAgainButton.addEventListener('click', function(e) {
+        playAgain();
+        showScore();
+        winner.removeChild(playAgainButton);
 
-    winner.textContent = "You've been bested by the computer!" 
-    + " Better luck next time...";
-
-    }
+        });
+    }   
 }
 
+function playAgain() {
+    playerScore = 0;
+    computerScore = 0;
+    winnerText.textContent = `The winner is the first to reach five points.`;
+}
 
+function showScore() {
+    score.textContent = 
+    `Score | Player: ${playerScore} 
+    Computer: ${computerScore}`;
+}
 
+let playerScore = 0;
+let computerScore = 0;
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', function(e) {
@@ -165,77 +172,6 @@ scissors.addEventListener('click', function(e) {
 });
 
 const results = document.querySelector('#results');
-
 const score = document.querySelector('#score');
 const winner = document.querySelector('#winner');
-
-
-
-
-
-
-
-
-
-// rock.addEventListener('click', () => {
-//   playMatch("Rock",getComputerChoice(3));
-// });
-
-
-
-
-
-
-// function game() {
-    
-//     let matchPrompt = prompt("Do you pick Rock, Paper or Scissors? ");
-//     let matchChoice = matchPrompt.toLowerCase();
-//     let computerChoice = getComputerChoice(3);
-    
-//     console.log(playMatch(matchChoice, computerChoice));
-
-//     matchPrompt = prompt("Do you pick Rock, Paper or Scissors? ");
-//     matchChoice = matchPrompt.toLowerCase();
-//     computerChoice = getComputerChoice(3);   
-
-//     console.log(playMatch(matchChoice, computerChoice));
-
-//     matchPrompt = prompt("Do you pick Rock, Paper or Scissors? ");
-//     matchChoice = matchPrompt.toLowerCase();
-//     computerChoice = getComputerChoice(3);
-    
-//     console.log(playMatch(matchChoice, computerChoice));
-
-//     matchPrompt = prompt("Do you pick Rock, Paper or Scissors? ");
-//     matchChoice = matchPrompt.toLowerCase();
-//     computerChoice = getComputerChoice(3); 
-
-//     console.log(playMatch(matchChoice, computerChoice));
-
-//     matchPrompt = prompt("Do you pick Rock, Paper or Scissors? ");
-//     matchChoice = matchPrompt.toLowerCase();
-//     computerChoice = getComputerChoice(3);
-   
-//     console.log(playMatch(matchChoice, computerChoice));
-    
-//     if (playerScore > computerScore){
-
-//         console.log("Congratulations! You beat the computer!");
-
-//     }
-
-//     else if (computerScore > playerScore) {
-//         console.log("You've been bested by the computer!" 
-//                 + " Better luck next time...")
-
-//     }
-
-//     else {
-
-//         console.log("You drew with the computer! Will you win next time?")
-//     }
-// }
-
-    
-// console.log(game());
-
+const winnerText = document.querySelector('#winnerText');
