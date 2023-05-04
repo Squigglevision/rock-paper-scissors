66c6b50dbe413dbb0193230fb39a9075b27d2ce3
Squigglevision <127502256+Squigglevision@users.noreply.github.com>
2023-05-04 15:15:51 +1000

    Refractor code and get abortController working
    "Play again" button now needs to be pressed in order for rock, paper
    and scissors buttons to be clickable on winning a set of five matches.
    
    Added css styling to buttons and text, and refractored code in
    JavaScript file. Refractoring included making eventlisteners its
    own function as it needed to be called when clicking the "Play Again"
    button.
    
    The code is still very clunky, particularly in the playMatch function,
    but it works. playMatch function could be more efficient as
    a loop, but it works.

--- javascript.js
+++ javascript.js
@@ -1,141 +1,129 @@
-
-function getComputerChoice(max) {
-    return Math.floor(Math.random() * max);
-}
-
 function playMatch(matchChoice, computerChoice) {
-
     if (matchChoice == "rock" && computerChoice === 0) {
-
-        console.log("You both picked Rock - it's a draw!"); 
-        results.textContent = 'You both picked Rock - it\'s a draw!';
         showScore();
-
+        results.textContent = 'You both picked Rock - it\'s a draw!';
     }
-
     else if (matchChoice == "rock" && computerChoice === 1) {
         computerScore = computerScore + 1;
         declareWinner();
-        console.log("You lose this round! Paper beats Rock");
-        results.textContent = 'You lose this round! Paper beats Rock';
         showScore();
-
-
-
+        results.textContent = 'You lose this round! Paper beats Rock';
     }
-
     else if (matchChoice == "rock" && computerChoice === 2) {
         playerScore = playerScore + 1;
         declareWinner();
-        console.log("You min this round! Rock beats scissors");
-        results.textContent = 'You win this round! Rock beats scissors';
         showScore();
-
-
+        results.textContent = 'You win this round! Rock beats scissors';
     }
-
-
     else if (matchChoice == "paper" && computerChoice === 0) {
         playerScore = playerScore + 1; 
         declareWinner();
-        console.log("You min this round! Paper beats rock");
+        showScore();
         results.textContent = 'You min this round! Paper beats rock';
-        showScore();
- 
-
     }
-
     else if (matchChoice == "paper" && computerChoice === 1) {
-
-        console.log("You both picked paper - it's a draw!");
-        results.textContent = 'You both picked paper - it\'s a draw!';
         showScore();
-
+        results.textContent = 'You both picked paper - it\'s a draw!';
     }
-
     else if (matchChoice == "paper" && computerChoice === 2) {
         computerScore = computerScore + 1;
         declareWinner();
-        console.log("You lose this round! scissors beats paper");
-        results.textContent = 'You lose this round! scissors beats paper';
         showScore();
-
-
+        results.textContent = 'You lose this round! scissors beats paper';
     }
-
     else if (matchChoice == "scissors" && computerChoice === 0) {
         computerScore = computerScore + 1; 
         declareWinner();
-        console.log("You lose this round! Rock beats scissors");
-        results.textContent = 'You lose this round! Rock beats scissors';
         showScore();
-
-
+        results.textContent = 'You lose this round! Rock beats scissors';
     }
-
     else if (matchChoice == "scissors" && computerChoice === 1) {
         playerScore = playerScore + 1; 
         declareWinner();
-        console.log("You min this round! Scissors beats paper");
+        showScore();
         results.textContent = 'You min this round! Scissors beats paper';
-        showScore();
-
-
     }
-
     else if (matchChoice == "scissors" && computerChoice === 2) {
-
-        console.log("You both picked scissors - it's a draw!");
+        showScore();
         results.textContent = 'You both picked scissors - it\'s a draw!';
-        showScore();
-
     }
-
     else {
-        console.log("Something strange happened...");
-        results.textContent = 'Hm... Something went wrong.';
         showScore();
+        results.textContent = 'Hm... Something went wrong.';
     }
 }
 
 function declareWinner() {
     if (playerScore === 5 && computerScore < 5){
-        console.log("Congratulations! You beat the computer!");
+        areaListener.abort() // Removes event listeners
         winnerText.textContent = `Congratulations! You beat the computer! \r\n`;
-        const playAgainButton = document.createElement('button');
-        winner.appendChild(playAgainButton); 
-        playAgainButton.innerText = `Play again?`;
-        playAgainButton.addEventListener('click', function(e) {
-        playAgain();
-        showScore();
-        winner.removeChild(playAgainButton);
-
-
-        });
-
+        makePlayAgainButton();
     }
-
     else if (computerScore === 5 && playerScore < 5) {
-        console.log("You've been bested by the computer!" 
-                + " Better luck next time...")
+        areaListener.abort() // Removes event listeners
         winnerText.textContent = `You've been bested by the computer!` 
         + ` Better luck next time... \r\n`;
-        const playAgainButton = document.createElement('button');
-        winner.appendChild(playAgainButton); 
-        playAgainButton.innerText = `Play again?`;
-        playAgainButton.addEventListener('click', function(e) {
+        makePlayAgainButton();
+    }   
+}
+
+function addEventListeners(){
+    areaListener = new AbortController(); // See orginal declaration for info
+    rock.addEventListener('click', function (e) {
+        rock.classList.add('btnClicked'); 
+        paper.classList.remove('btnClicked');
+        scissors.classList.remove('btnClicked');
+        let computerChoice = getComputerChoice(3);
+        matchChoice = "rock";
+        playMatch(matchChoice, computerChoice)
+        },
+        { signal: areaListener.signal }
+    );
+    paper.addEventListener('click', function(e) {
+        paper.classList.add('btnClicked');
+        rock.classList.remove('btnClicked'); 
+        scissors.classList.remove('btnClicked');
+
+        let computerChoice = getComputerChoice(3);
+        matchChoice = "paper";
+        playMatch(matchChoice, computerChoice)
+        },
+        { signal: areaListener.signal }
+    );
+    scissors.addEventListener('click', function(e) {
+        scissors.classList.add('btnClicked'); 
+        paper.classList.remove('btnClicked');
+        rock.classList.remove('btnClicked'); 
+
+        let computerChoice = getComputerChoice(3);
+        matchChoice = "scissors";
+        playMatch(matchChoice, computerChoice)
+        },
+        { signal: areaListener.signal }
+    );
+}
+
+function makePlayAgainButton() {
+    const playAgainButton = document.createElement('button');
+    playAgainButton.classList.add('playAgain'); 
+    winner.appendChild(playAgainButton); 
+    playAgainButton.innerText = `Play again?`;
+    playAgainButton.addEventListener('click', function(e) {
         playAgain();
         showScore();
         winner.removeChild(playAgainButton);
-
-        });
-    }   
+    });
 }
 
 function playAgain() {
     playerScore = 0;
     computerScore = 0;
-    winnerText.textContent = `The winner is the first to reach five points.`;
+    winnerText.textContent = `Play rock, paper, scissors!
+    The winner is the first to reach five points.`;
+    rock.classList.remove('btnClicked'); 
+    paper.classList.remove('btnClicked');
+    scissors.classList.remove('btnClicked');
+    addEventListeners();
 }
 
 function showScore() {
@@ -144,34 +132,29 @@ function showScore() {
     Computer: ${computerScore}`;
 }
 
+function getComputerChoice(max) {
+    return Math.floor(Math.random() * max);
+}
+
 let playerScore = 0;
 let computerScore = 0;
 
 const rock = document.querySelector('#rock');
-rock.addEventListener('click', function(e) {
-    let computerChoice = getComputerChoice(3);
-    matchChoice = "rock";
-    console.log("rock");
-    playMatch(matchChoice, computerChoice)
-});
-
 const paper = document.querySelector('#paper');
-paper.addEventListener('click', function(e) {
-    let computerChoice = getComputerChoice(3);
-    matchChoice = "paper";
-    console.log("paper");
-    playMatch(matchChoice, computerChoice)
-});
-
 const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', function(e) {
-    let computerChoice = getComputerChoice(3);
-    matchChoice = "scissors";
-    console.log("scissors");
-    playMatch(matchChoice, computerChoice)
-});
 
 const results = document.querySelector('#results');
 const score = document.querySelector('#score');
 const winner = document.querySelector('#winner');
 const winnerText = document.querySelector('#winnerText');
+
+let areaListener = new AbortController(); 
+// AbortController() provides the ability to abort event listeners
+// The addEventListener() function makes a new AbortController() every time
+// it's called so that events are not always in "aborted" state
+// signal: areaListener.signal looks for variable areaListener after event and
+// allows areaListener.abort to be called on each event with areaListener.signal
+// All this just to make the buttons not clickable until you click "Play again"
+// if the game ends, haha
+
+addEventListeners();
